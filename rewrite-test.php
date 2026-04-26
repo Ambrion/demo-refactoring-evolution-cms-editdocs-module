a966dc1d66aca12ceab0b94a96e3f8de43eb7c8e
Ambrion <brikov.igor@gmail.com>
2026-04-26 16:48:07 +0300

    chore: Убрал лишний файл rewrite-test.php

--- rewrite-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<h1>✅ Rewrite работает!</h1>';
-echo '<pre>';
-echo 'REQUEST_URI: ' . $_SERVER['REQUEST_URI'] . "\n";
-echo 'QUERY_STRING: ' . $_SERVER['QUERY_STRING'] . "\n";
-echo 'q parameter: ' . ($_GET['q'] ?? 'NOT SET') . "\n";
-echo 'DOCUMENT_ROOT: ' . $_SERVER['DOCUMENT_ROOT'] . "\n";
-echo '</pre>';
